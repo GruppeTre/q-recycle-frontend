f1ff092350e841801e61b8ad96a61792d46f0a9b
MaVirgil <magnusvirgil@hotmail.com>
2026-05-11 13:31:34 +0200

    add basic styling

--- src/features/partner-login/PartnerLoginPage.jsx
+++ src/features/partner-login/PartnerLoginPage.jsx
@@ -1,21 +1,26 @@
 import {INTERNAL_EMAIL_SUFFIX} from "../../config/constants.js";
 import {passwordSignIn} from "../../lib/supabaseUtils.js";
+import PartnerSignInForm from "./components/PartnerSignInForm.jsx";
 
 function PartnerLoginPage() {
+
     const handleSignIn = async (formData) => {
 
         const pin = formData.get('pin');
         const email = pin + INTERNAL_EMAIL_SUFFIX;
 
         const data = await passwordSignIn(email, pin);
+
+        console.log(JSON.stringify(data));
     }
+
     return (
-        <>
-            <form action={formData => handleSignIn(formData)}>
-                <input type={'text'} placeholder={'PIN'} name={'pin'}></input>
-                <button type={'submit'}>LOG IND</button>
-            </form>
-        </>
+        <div className={'w-lg mx-auto'}>
+            <div className={'w-full flex flex-col gap-2 items-center mt-4'}>
+                <h2 className={'text-lg text-gray-600'}>Indtast din pinkode:</h2>
+                <PartnerSignInForm callback={(formData) => handleSignIn(formData)}/>
+            </div>
+        </div>
     );
 }
 
