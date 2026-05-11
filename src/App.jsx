fe684d7b404d66ebe2eb1955091a89cedcf21909
MaVirgil <magnusvirgil@hotmail.com>
2026-05-12 00:43:18 +0200

    add protected route component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter, Route, Routes} from "react-router";
 import FrontPage from "./features/front-page";
 import PartnerLoginPage from "./features/partner-login/PartnerLoginPage.jsx";
 import DriverAdminLoginPage from "./features/driver-admin-login/DriverAdminLoginPage.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
 function App() {
 
@@ -10,7 +11,10 @@ function App() {
           <Routes>
               <Route index element={<FrontPage />}/>
               <Route path="driverlogin" element={<DriverAdminLoginPage />}/>
-              <Route path={'partner'} element={<PartnerLoginPage/>}/>
+              <Route path={'partner'} element={<ProtectedRoute redirectPath={'/'} />}>
+                  <Route index element={<PartnerLoginPage />} />
+                  <Route path={'dashboard'} element={<h1>Partner dashboard!</h1>} />
+              </Route>
           </Routes>
       </BrowserRouter>
   );
