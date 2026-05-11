394f2e2de188f70a42fa047dd4490d35d87088f9
MaVirgil <magnusvirgil@hotmail.com>
2026-05-12 01:20:21 +0200

    fix code style

--- src/features/partner-login/components/PartnerSignInForm.jsx
+++ src/features/partner-login/components/PartnerSignInForm.jsx
@@ -3,9 +3,9 @@ function PartnerSignInForm({ callback }) {
     return (
         <div>
             <form action={formData => callback(formData)}>
-                <div className={'flex gap-2'}>
-                    <input type={'text'} placeholder={'PIN'} name={'pin'} className={'bg-gray-200 p-4 h-full rounded-md'}></input>
-                    <button type={'submit'} className={'p-4 bg-blue-400 rounded-md'}>LOG IND</button>
+                <div className="flex gap-2">
+                    <input type="password" minLength="5" placeholder="PIN" name="pin" className="bg-gray-200 p-4 h-full rounded-md"></input>
+                    <button type="submit" className="p-4 bg-blue-400 rounded-md">LOG IND</button>
                 </div>
             </form>
         </div>
