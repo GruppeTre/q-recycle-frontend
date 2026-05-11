fd4048ab5d176d1e663c61b6c9cceeb0b126ba33
MaVirgil <magnusvirgil@hotmail.com>
2026-05-11 12:33:01 +0200

    move partner-login to discrete component

--- src/features/front-page/FrontPage.jsx
+++ src/features/front-page/FrontPage.jsx
@@ -1,27 +1,7 @@
-import {INTERNAL_EMAIL_SUFFIX} from "../../config/constants.js";
-import {passwordSignIn} from "../../lib/supabaseUtils.js";
-
 function FrontPage() {
 
-
-    const handleSignIn = async (formData) => {
-
-        const pin = formData.get('pin');
-        const email = pin + INTERNAL_EMAIL_SUFFIX;
-
-        const data = await passwordSignIn(email, pin);
-
-        console.log(JSON.stringify(data));
-
-        console.log(JSON.stringify(data));
-    }
     return (
-        <>
-            <form action={formData => handleSignIn(formData)}>
-                <input type={'text'} placeholder={'PIN'} name={'pin'}></input>
-                <button type={'submit'}>LOG IND</button>
-            </form>
-        </>
+        <h1>Forside</h1>
     );
 }
 
---
+++ src/features/partner-login/PartnerLoginPage.jsx
@@ -0,0 +1,22 @@
+import {INTERNAL_EMAIL_SUFFIX} from "../../config/constants.js";
+import {passwordSignIn} from "../../lib/supabaseUtils.js";
+
+function PartnerLoginPage() {
+    const handleSignIn = async (formData) => {
+
+        const pin = formData.get('pin');
+        const email = pin + INTERNAL_EMAIL_SUFFIX;
+
+        const data = await passwordSignIn(email, pin);
+    }
+    return (
+        <>
+            <form action={formData => handleSignIn(formData)}>
+                <input type={'text'} placeholder={'PIN'} name={'pin'}></input>
+                <button type={'submit'}>LOG IND</button>
+            </form>
+        </>
+    );
+}
+
+export default PartnerLoginPage;
