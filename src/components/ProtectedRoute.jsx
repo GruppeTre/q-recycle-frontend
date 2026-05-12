a6c8bae96076e15dacdae5a489fcead42bd4b13b
MaVirgil <magnusvirgil@hotmail.com>
2026-05-12 11:55:21 +0200

    add role authorization

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,27 +1,33 @@
 import {useEffect, useState} from "react";
 import {Navigate, Outlet} from "react-router";
 import {supabaseClient} from "../lib/supabaseClient.js";
+import {getRole} from "../lib/supabaseUtils.js";
 
 function ProtectedRoute({redirectPath, allowedRoles = [], children}) {
 
     const [session, setSession] = useState(undefined); // undefined = still loading
+    const [userRole, setUserRole] = useState(undefined);
 
     useEffect(() => {
-        supabaseClient.auth.getSession().then(({ data }) => {
-            setSession(data.session);
+        supabaseClient.auth.getSession().then(async ({ data }) => {
+            const s = data.session;
+            setSession(s);
+            if (s) {
+                const role = await getRole(s.user.id);
+                setUserRole(role);
+            }
         });
     }, []);
 
-    //placeholder loading indicator
-    if (session === undefined) return <h1>LOADING</h1>;
+    if (session === undefined || (session && userRole === undefined)) return <h1>LOADING</h1>;
 
-    //if user is not logged in / does not have the correct role
-    if (session === null || !allowedRoles.includes(session.user.role)) {
+    if (session === null) {
         return <Navigate to={redirectPath} replace />;
     }
 
-    //if user does not have the correct role
-    console.log(session.user.role);
+    if (!allowedRoles.includes(userRole)) {
+        return <Navigate to={redirectPath} replace />;
+    }
 
     return children ? children : <Outlet />;
 }
