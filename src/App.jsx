5fcecd04d52cbb3a66a6f7aacfd39419f9942cc8
MaVirgil <magnusvirgil@hotmail.com>
2026-05-12 09:12:04 +0200

    fix missing redirect path

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
               <Route path="partner" >
                   <Route index="/login" element={<PartnerLoginPage />} />
                   <Route path="dashboard" element={
-                      <ProtectedRoute><h1>DASHBOARD</h1></ProtectedRoute>
+                      <ProtectedRoute redirectPath="/"><h1>DASHBOARD</h1></ProtectedRoute>
                   } />
               </Route>
           </Routes>
