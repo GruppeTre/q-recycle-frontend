9b0681a9ae975db071bbb5ce625904f6e2c62b5e
MaVirgil <magnusvirgil@hotmail.com>
2026-05-13 11:12:37 +0200

    fix protected routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import {BrowserRouter, Route, Routes} from "react-router";
-import FrontPage from "./features/front-page";
 import PartnerLoginPage from "./features/partner-login-page/PartnerLoginPage.jsx";
 import UserLoginPage from "./features/user-login-page/UserLoginPage.jsx";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
@@ -10,31 +9,22 @@ function App() {
   return (
       <BrowserRouter>
           <Routes>
-              <Route index element={<FrontPage />}/>
-              <Route path="login" element={<UserLoginPage />}/>
+              <Route index element={<UserLoginPage />}/>
+
               <Route path="partner" >
                   <Route index="/login" element={<PartnerLoginPage />} />
                   <Route path="dashboard" element={
-                      <ProtectedRoute redirectPath="/" allowedRoles={[role.PARTNER]}><h1>DASHBOARD</h1></ProtectedRoute>
+                      <ProtectedRoute redirectPath="/partner/login" allowedRoles={[role.PARTNER]}><h1>DASHBOARD</h1></ProtectedRoute>
                   } />
               </Route>
 
-              <Route path="driver">
-                  <Route path="login" element={<UserLoginPage />} />
-                  <Route path="dashboard" element={
-                      <ProtectedRoute redirectPath="/driver/login" allowedRoles={[role.DRIVER]}>
-                          <h1>DRIVER DASHBOARD</h1>
-                      </ProtectedRoute>
-                  } />
+              <Route path="driver" element={<ProtectedRoute redirectPath="/" allowedRoles={[role.DRIVER, role.ADMIN]}/>}>
+                  <Route path="dashboard" element={<h1>DRIVER DASHBOARD</h1>} />
               </Route>
 
-              <Route path="admin">
+              <Route path="admin" element={<ProtectedRoute redirectPath="/" allowedRoles={[role.ADMIN]}/>}>
                   <Route path="login" element={<UserLoginPage />} />
-                  <Route path="dashboard" element={
-                      <ProtectedRoute redirectPath="/admin/login" allowedRoles={[role.ADMIN]}>
-                          <h1>ADMIN DASHBOARD</h1>
-                      </ProtectedRoute>
-                  } />
+                  <Route path="dashboard" element={<h1>ADMIN DASHBOARD </h1>} />
               </Route>
           </Routes>
       </BrowserRouter>
