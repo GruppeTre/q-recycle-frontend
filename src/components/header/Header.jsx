4fce56749be7b9faec23378302de37dd2f21ee25
MaVirgil <magnusvirgil@hotmail.com>
2026-05-15 18:33:23 +0200

    add hero icon to header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,15 @@
-import Container from "../Container.jsx";
+import HeroIcon from "./components/HeroIcon.jsx";
 
 function Header({title, navItems}) {
     return (
-        <nav className="bg-background">
-            <Container>
-                <div className="flex justify-between">
-                    <div className="flex gap-2">
-                        <p>LOGO</p>
+        <nav className="bg-background px-gap-lg py-gap-md border-b border-surface">
+                <div className="flex justify-between items-center">
+                    <div className="flex gap-2 items-center">
+                        <HeroIcon />
                         <p>{title}</p>
                     </div>
                     <p>HAMBURGER MENU</p>
                 </div>
-            </Container>
         </nav>
     );
 }
---
+++ src/components/header/components/HeroIcon.jsx
@@ -0,0 +1,9 @@
+function HeroIcon() {
+    return (
+        <div className="bg-primary rounded-lg p-gap-sm">
+            <img src="/src/assets/hero-icon.png" alt="logo" className="w-4 h-4"/>
+        </div>
+    );
+}
+
+export default HeroIcon;
