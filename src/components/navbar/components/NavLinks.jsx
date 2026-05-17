13226eb211fcba2e5231391891549dc3ee939e02
MaVirgil <magnusvirgil@hotmail.com>
2026-05-17 19:24:37 +0200

    fix navbar links not indicating current page

--- src/components/navbar/components/NavLinks.jsx
+++ src/components/navbar/components/NavLinks.jsx
@@ -1,11 +1,14 @@
-import {Link} from "react-router";
+import {Link, useLocation} from "react-router";
 
 function NavLinks({navItems}) {
+
+    const location = useLocation();
+
     return (
         <>
             {navItems.map(navItem =>
                 <Link key={navItem.label} to={navItem.to}>
-                    <div className="flex gap-gap-sm px-gap-md py-gap-sm w-full rounded-md bg-primary hover:bg-primary-hover">
+                    <div className={`flex gap-gap-sm px-gap-md py-gap-sm w-full rounded-md ${navItem.to === location.pathname ? 'bg-primary' : 'bg-surface'}`}>
                         {navItem.icon}
                         <p className="text-text-body text-center">{navItem.label}</p>
                     </div>
