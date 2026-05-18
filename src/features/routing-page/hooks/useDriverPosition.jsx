bd701d0ed27a9803f4632e52314a3ac5ec3b6984
WiiTee <somewhereinnowhere@live.dk>
2026-05-18 09:24:50 +0200

    Setup of Driver position hook using gps location

---
+++ src/features/routing-page/hooks/useDriverPosition.jsx
@@ -0,0 +1,31 @@
+import {useState, useEffect} from "react";
+
+export function useDriverPosition(enabled = true) {
+    const [position, setPosition] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        if (!enabled) return;
+        if (!("geolocation" in navigator)) {
+            setError("Geolocation is not supported");
+            return
+        }
+
+        const watchId = navigator.geolocation.watchPosition(
+            (pos) => {
+                setPosition(pos.coords.longitude, pos.coords.latitude);
+                setError(null);
+            },
+            (err) => setError(err.message),
+            {
+                enableHighAccuracy: true,
+                maximumAge: 5000,
+                timeout: 15000,
+            }
+        );
+
+        return () => navigator.geolocation.clearWatch(watchId);
+    }, [enabled]);
+
+    return {position, error};
+}
