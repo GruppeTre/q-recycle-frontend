a06a42a6b4116351f066ba4ad0f8e44a6485f278
MaVirgil <magnusvirgil@hotmail.com>
2026-05-18 13:01:49 +0200

    add section card component

--- src/components/SectionCard.jsx
+++ src/components/SectionCard.jsx
@@ -1,6 +1,11 @@
-function SectionCard({children}) {
+function SectionCard({children, title = null}) {
     return (
-        <div className="flex flex-col gap-gap-md bg-background rounded-lg px-gap-md py-gap-lg shadow-sm">
+        <div className="flex flex-col gap-gap-md bg-background rounded-lg px-gap-md py-gap-md shadow-sm">
+            {title &&
+                <div className="w-full border-b border-surface">
+                    <h2 className="text-section-header">{title}</h2>
+                </div>
+            }
             {children}
         </div>
     );
