abce57d2c47c664a1bb6e03a427b1e6ece49ac0d
MaVirgil <magnusvirgil@hotmail.com>
2026-05-19 09:41:57 +0200

    finish expandable section card component

--- src/components/SectionCardExpandable.jsx
+++ src/components/SectionCardExpandable.jsx
@@ -1,16 +1,25 @@
 import {useState} from "react";
+import {ChevronDown, ChevronUp} from "lucide-react";
 
-function SectionCardExpandable({children, title = null, initialState}) {
+function SectionCardExpandable({children, title = null, initialIsOpen}) {
 
-    const [isOpen, setIsOpen] = useState(initialState)
+    const [isOpen, setIsOpen] = useState(initialIsOpen);
+
+    const handleAccordionToggle = () => {
+        setIsOpen(prevState => !prevState);
+    }
+
     return (
         <div className="flex flex-col gap-gap-md bg-background rounded-lg px-gap-md py-gap-md shadow-sm">
             {title &&
-                <div className="w-full border-b border-surface">
-                    <h2 className="text-section-header">{title}</h2>
-                </div>
+                <button className="cursor-pointer" onClick={handleAccordionToggle}>
+                    <div className={`flex justify-between w-full ${isOpen ? 'border-b' : ''} border-surface`}>
+                        <h2 className="text-section-header">{title}</h2>
+                            {isOpen ? <ChevronUp /> : <ChevronDown />}
+                    </div>
+                </button>
             }
-            {children}
+            {isOpen && children}
         </div>
     );
 }
