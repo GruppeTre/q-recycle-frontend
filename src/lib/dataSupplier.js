60fda28c598013a0777a2669f2adf05900483348
MaVirgil <magnusvirgil@hotmail.com>
2026-05-19 11:10:20 +0200

    add dummy data for data visualization

---
+++ src/lib/dataSupplier.js
@@ -0,0 +1,36 @@
+export const pickupData = [
+    {
+        name: '03/04/26',
+        pickups: 14,
+    },
+    {
+        name: '09/04/26',
+        pickups: 12,
+    },
+    {
+        name: '17/04/26',
+        pickups: 17,
+    },
+    {
+        name: '23/04/26',
+        pickups: 18,
+    },
+    {
+        name: '30/04/26',
+        pickups: 28,
+    },
+    {
+        name: '01/05/26',
+        pickups: 16,
+    },
+    {
+        name: '07/05/26',
+        pickups: 21,
+    },    {
+        name: '13/05/26',
+        pickups: 15,
+    },    {
+        name: '21/05/26',
+        pickups: 19,
+    },
+]
