5dc4fcfa20ba9c1d4eccc4911bd667efd21addf2
Isabellabmj <Isabellajoensen@gmail.com>
2026-05-19 12:28:45 +0200

    removed maybeSingle from getActive

--- src/lib/pickupRequest.js
+++ src/lib/pickupRequest.js
@@ -1,20 +1,42 @@
 import {supabaseClient} from "./supabaseClient.js";
 import {pickupStatus} from "../config/constants.js";
 
+function handlePostgresError(postgresError) {
+    if (postgresError) {
+        const error = new Error(postgresError.message);
+        error.code = postgresError.code;
+        throw error;
+    }
+}
+
+
 export const pickupRequest = {
     getActive: async (userId) => {
-        const { data, error: postgresError } = await supabaseClient
+
+        const {data, error: postgresError} = await supabaseClient
             .from("pickup")
             .select("*")
             .eq("partner_id", userId)
             .eq("status", pickupStatus.REQUESTED)
-            .maybeSingle()
 
-        if(postgresError) {
-            const error = new Error(postgresError.message);
-            error.code = postgresError.code;
-            throw error;
-        }
-        return data ?? null;
-    }
+        console.log(JSON.stringify(data, null, 2))
+
+        handlePostgresError(postgresError);
+
+        return data;
+
+    },
+
+
+    cancelActive: async (userId) => {
+        const {error: postgresError} = await supabaseClient
+            .from("pickup")
+            .update({status: pickupStatus.CANCELLED})
+            .eq("partner_id", userId)
+            .eq("status", pickupStatus.REQUESTED)
+
+        handlePostgresError(postgresError);
+
+    },
+
 }
