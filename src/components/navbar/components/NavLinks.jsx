376f0fb4cf2af4d16cac1af52278a499b70565f9
MaVirgil <magnusvirgil@hotmail.com>
2026-05-21 11:39:35 +0200

    fix bad behavior when link text is too long

--- src/components/navbar/components/NavLinks.jsx
+++ src/components/navbar/components/NavLinks.jsx
@@ -7,10 +7,12 @@ function NavLinks({navItems}) {
     return (
         <>
             {navItems.map(navItem =>
-                <Link key={navItem.label} to={navItem.to} className="w-full">
-                    <div className={`flex gap-gap-sm px-gap-md py-gap-sm rounded-md ${navItem.to === location.pathname ? 'bg-primary' : 'bg-surface'}`}>
-                        {navItem.icon}
-                        <p className="text-text-body text-center">{navItem.label}</p>
+                <Link key={navItem.label} to={navItem.to} className="w-full md:max-w-48">
+                    <div className={`flex gap-gap-sm px-gap-md py-gap-sm rounded-md whitespace-nowrap ${navItem.to === location.pathname ? 'bg-primary' : 'bg-surface'}`}>
+                        <span className="shrink-0">{navItem.icon}</span>
+                        <div className="overflow-hidden">
+                            <p className="text-text-body text-center">{navItem.label}</p>
+                        </div>
                     </div>
                 </Link>
             )}
