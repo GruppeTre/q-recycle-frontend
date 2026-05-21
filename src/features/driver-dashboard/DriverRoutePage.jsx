e608ecccea72c4db41efd8b2bcedef376ad52945
WiiTee <somewhereinnowhere@live.dk>
2026-05-21 11:29:56 +0200

    moved routing into driver-dashboard

--- src/features/driver-dashboard/DriverRoutePage.jsx
+++ src/features/driver-dashboard/DriverRoutePage.jsx
@@ -1,10 +1,14 @@
 import PageContainer from "../../components/PageContainer.jsx";
+import SectionCard from "../../components/SectionCard.jsx";
 
 function DriverRoutePage() {
 
     return (
         <PageContainer>
             <h1 className="text-section-header">Rute Oversigt</h1>
+            <SectionCard>
+
+            </SectionCard>
 
         </PageContainer>
     )
