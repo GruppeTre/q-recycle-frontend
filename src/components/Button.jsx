7ca483d6562213cfa4427089ef906d333eae6462
MaVirgil <magnusvirgil@hotmail.com>
2026-05-22 08:21:10 +0200

    add optional icon component to button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,23 @@
-function Button({onClick,type = "button", children}) {
+function Button({onClick,type = "button", children, icon = null}) {
     return(
-        <button  type={type} onClick={onClick} className="rounded-md px-gap-md py-gap-sm bg-primary text-text-body cursor-pointer
-                 hover:bg-primary-hover">{children}</button>
+        <button
+            type={type}
+            onClick={onClick}
+            className="
+                rounded-md
+                px-gap-md
+                py-gap-sm
+                text-text-body
+                cursor-pointer
+                bg-primary
+                hover:bg-primary-hover
+            "
+        >
+            <div className="flex gap-gap-sm whitespace-nowrap">
+                {icon && icon}
+                {children}
+            </div>
+        </button>
     );
 }
 
