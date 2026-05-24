e4544055d567bfe753eeebf5410e7951ed699ddc
MaVirgil <magnusvirgil@hotmail.com>
2026-05-24 23:48:28 +0200

    styling changes

--- src/features/driver-dashboard/pages/DriverExpendituresPage/components/ExpenditureCard.jsx
+++ src/features/driver-dashboard/pages/DriverExpendituresPage/components/ExpenditureCard.jsx
@@ -1,5 +1,5 @@
 import SectionCard from "../../../../../components/SectionCard.jsx";
-import {ChevronDown, CircleDot, EyeIcon, PenIcon, ReceiptText, Trash, Trash2} from "lucide-react";
+import {ChevronDown, CircleDot, CircleDotDashed, EyeIcon, PenIcon, ReceiptText, Trash, Trash2} from "lucide-react";
 import Button from "../../../../../components/Button.jsx";
 
 function ExpenditureCard({ expenditure, selected = false, onClick}) {
@@ -10,53 +10,66 @@ function ExpenditureCard({ expenditure, selected = false, onClick}) {
                 <SectionCard backgroundColor="surface-secondary">
                     <div className="flex flex-col gap-gap-sm">
                         <div className="flex justify-between">
-                            <div className="flex justify-between w-full">
-                                <div className="flex flex-col gap-gap-sm w-1/3 overflow-hidden">
-                                    <h3 className="font-semibold ">{expenditure.name}</h3>
-                                    <div className={`flex gap-gap-sm ${expenditure.is_pending ? 'text-danger' : 'text-success'}`}>
-                                        <CircleDot />
-                                        <p>{expenditure.is_pending
-                                            ? 'Ubetalt'
-                                            : 'Betalt'
-                                        }</p>
-                                    </div>
-                                </div>
-                                <div className="flex justify-center items-end -mb-3">
-                                    <ChevronDown className={`transition-all duration-300 ${selected ? 'rotate-180' : ''}`}/>
-                                </div>
-                                <div className="flex flex-col gap-gap-sm items-end w-1/3">
-                                    <h3 className='font-semibold text-text-color'>{expenditure.amount} kr</h3>
-                                    <p className="text-muted">{expenditure.created_at}</p>
-                                </div>
+                            <h3 className="font-semibold ">{expenditure.name}</h3>
+                            <h3 className='font-semibold text-text-color'>{expenditure.amount} kr</h3>
+                        </div>
+                        <div className="flex justify-between">
+                            <div>
+                                <p className="text-muted">{expenditure.created_at}</p>
+                            </div>
+                            <div className={`flex items-end gap-gap-sm ${expenditure.is_pending ? 'text-primary' : 'text-success'}`}>
+                                <p>{expenditure.is_pending
+                                    ? 'Afventer'
+                                    : 'Refunderet'
+                                }
+                                </p>
+                                {expenditure.is_pending
+                                    ? <CircleDotDashed />
+                                    : <CircleDot />
+                                }
                             </div>
                         </div>
+                        <div className="flex justify-center items-end -mb-3">
+                            <ChevronDown className={`transition-all duration-300 ${selected ? 'rotate-180' : ''}`}/>
+                        </div>
+                        {/*    <div className="flex justify-between w-full">*/}
+                        {/*        <div className="flex flex-col gap-gap-sm w-1/3 overflow-hidden">*/}
+
+                        {/*        </div>*/}
+
+                        {/*        <div className="flex flex-col gap-gap-sm items-end w-1/3">*/}
+                        {/*            <h3 className='font-semibold text-text-color'>{expenditure.amount} kr</h3>*/}
+                        {/*            <p className="text-muted">{expenditure.created_at}</p>*/}
+                        {/*        </div>*/}
+                        {/*    </div>*/}
+                        {/*</div>*/}
                     </div>
                 </SectionCard>
             </div>
             <div className={`relative z-0 -translate-y-3 w-full grid transition-[grid-template-rows] duration-200 ${selected ? 'grid-rows-[1fr]' : 'grid-rows-[0fr]'}`}>
-                <div className="overflow-hidden min-h-0 shadow-sm rounded-lg">
+                <div className="overflow-hidden min-h-0 shadow-sm rounded-lg ">
                     <SectionCard backgroundColor="surface-secondary">
-                        <div className="flex justify-between md:max-w-44">
+                        <div className="flex justify-between md:max-w-44 ">
                             <button
                                 type="button"
                                 className="p-2 text-text-color cursor-pointer w-min"
                                 aria-label="Slet partner"
                             >
-                                <ReceiptText size={28} />
+                                <ReceiptText size={24} />
                             </button>
                             <button
                                 type="button"
                                 className="p-2 text-text-color cursor-pointer w-min"
                                 aria-label="Slet partner"
                             >
-                                <PenIcon size={28} />
+                                <PenIcon size={24} />
                             </button>
                             <button
                                 type="button"
                                 className="p-2 text-danger cursor-pointer w-min"
                                 aria-label="Slet partner"
                             >
-                                <Trash2 size={28} />
+                                <Trash2 size={24} />
                             </button>
 
                         </div>
