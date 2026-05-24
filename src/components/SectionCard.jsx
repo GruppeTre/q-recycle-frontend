57d992a1ec3769677069d0033f48810ccdbb86af
MaVirgil <magnusvirgil@hotmail.com>
2026-05-24 23:23:56 +0200

    add optional border prop

--- src/components/SectionCard.jsx
+++ src/components/SectionCard.jsx
@@ -1,6 +1,18 @@
-function SectionCard({children, title = null, headerContent = null, backgroundColor = null}) {
+function SectionCard({children, title = null, headerContent = null, backgroundColor = null, border = false}) {
     return (
-        <div className={`flex flex-col gap-gap-md rounded-lg px-gap-md py-gap-md shadow-sm ${backgroundColor ? `bg-${backgroundColor}` : 'bg-surface-primary'}`}>
+        <div className={`
+            flex
+            flex-col
+            gap-gap-md
+            rounded-lg
+            px-gap-md
+            py-gap-md
+            shadow-sm
+            box-border
+            border-2
+            ${backgroundColor ? `bg-${backgroundColor}` : 'bg-surface-primary'}
+            ${border ? 'border-primary' : 'border-transparent'}
+        `}>
             {(title || headerContent) &&
                 <div className="w-full border-b border-surface-primary-accent pb-gap-sm">
                     {title && <h2 className="text-section-header">{title}</h2>}
