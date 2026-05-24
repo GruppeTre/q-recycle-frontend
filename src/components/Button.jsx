efac63e5e2def977c36f9cbe3b6e46457fb06476
MaVirgil <magnusvirgil@hotmail.com>
2026-05-24 23:23:37 +0200

    add optional color prop

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,17 @@
-function Button({onClick,type = "button", children, icon = null}) {
+function Button({onClick,type = "button", children, icon = null, backgroundColor = null}) {
     return(
         <button
             type={type}
             onClick={onClick}
-            className="
+            className={`
                 rounded-md
                 px-gap-md
                 py-gap-sm
                 text-text-body
                 cursor-pointer
-                bg-primary
+                ${backgroundColor ? `bg-${backgroundColor}` : 'bg-primary'}
                 hover:bg-primary-hover
-            "
+            `}
         >
             <div className="flex justify-center gap-gap-sm whitespace-nowrap">
                 {icon && icon}
