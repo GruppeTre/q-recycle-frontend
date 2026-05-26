ccd1b0cf08860880c6fb289de3e66ec0970a2623
MaVirgil <magnusvirgil@hotmail.com>
2026-05-26 11:49:13 +0200

    fix pending pickups badge not showing

--- src/features/driver-dashboard/DriverDashboard.jsx
+++ src/features/driver-dashboard/DriverDashboard.jsx
@@ -1,13 +1,16 @@
 import Navbar from "../../components/navbar/Navbar.jsx"
 import {Outlet} from "react-router";
 import { driverNavItems } from "./DriverNavItems.jsx";
+import {useRequestedBagsCount} from "../admin-dashboard/hooks/useRequestedBagsCount.js";
 
 function DriverDashboard() {
+
+    const { count } = useRequestedBagsCount();
 
     return (
         <>
             <div>
-                <Navbar title="Chauffør" navItems={driverNavItems} />
+                <Navbar title="Chauffør" navItems={driverNavItems} pendingBags={count}/>
                 <Outlet />
             </div>
         </>
